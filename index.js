f0d65d77239a545e95d743bb5487763754fb9c5e
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2023-02-20 10:03:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express()
+const cors = require ("cors");
+const path = require("path");
+require('dotenv').config()
+
+
+//app.use(cors({ origin: "*", credentials: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//app.use(express.static('public/dist'))
+
+
+app.get("/", function(req, res){
+    return res.json({status: 200})
+})
+
+app.post("/", function(req,res){
+
+})
+app.put("/", function(req,res){
+
+})
+app.delete("/", function(req,res){
+
+})
+
+/*
+app.get('/', (req, res) => {
+	return res.sendFile(path.join(__dirname, "./public/dist", "index.html"));
+});*/
+
+app.listen(process.env.PORT || 3000, function(){console.log("Server started")});
